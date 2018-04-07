0832551c23bee3538be590106990ff567fc49c4f
Domas <domas.bitvinskas@me.com>
2018-04-08 00:26:35 +0300

    Refactor a bit

--- index.js
+++ index.js
@@ -1,86 +1,15 @@
-const _ = require('lodash')
-const { everySeries } = require('p-iteration');
-const gql = require('graphql-tag')
-const queryServer = require('./queryServer');
+const queryServer = require('./lib/queryServer');
+const get = require('./lib/get');
+const create = require('./lib/create');
+const and = require('./lib/and');
 
-global.definedFunctions = [];
-
-const get = async function(name, variables) {
-  const definedFunction = _.find(global.definedFunctions, { name });
-  const outputAttributes = _.keys(definedFunction.output);
-
-  const inputTypes = (fn) => {
-    const result = [];
-
-    _.each(fn.input, (val, key) => {
-      if (_.includes(_.keys(variables), key)) result.push([`$${key}: ${val}`]);
-
-      const dependency = _.find(global.definedFunctions, { name: val.replace(/Input$/, '') });
-
-      if (!dependency) return;
-
-      _.each(dependency.input, (depVal, depKey) => {
-        if (_.includes(_.keys(variables), depKey)) result.push([`$${depKey}: ${depVal}`]);
-      });
-    });
-
-    return result.join(', ');
-  };
-
-  const functionArgs = 'image: $image';
-
-  const query = gql`
-    query(${inputTypes(definedFunction)}) {
-      ${name}(${functionArgs}) {
-        ${outputAttributes.join("\n")}
-      }
-    }
-  `;
-
-  const result = await queryServer({
-    query,
-    variables,
-  });
-
-  return _.get(result, `data.${name}`);
-};
+global.definitions = [];
 
 module.exports = (opts) => {
   return {
-    and: function() {
-      const functions = arguments;
-
-      _.each(functions, (fn) => {
-        global.definedFunctions[fn.name] = (innerVariables) => fn(innerVariables);
-      });
-
-      const result = (variables) => {
-        global.definedFunctions[opts.name] = async (innerVariables) => {
-          return everySeries(functions, async (fn) => {
-            return await get(fn.name, variables);
-          });
-        };
-
-        return global.definedFunctions[opts.name](variables);
-      };
-
-      Object.defineProperty(result, 'name', { value: opts.name });
-      return result;
-    },
-    create: function(fn, opts) {
-      global.definedFunctions.push({
-        fn: (innerVariables) => fn(innerVariables),
-        ...opts,
-      });
-
-      const result = async (variables) => {
-        return await get(fn.name, variables);
-      };
-
-      Object.defineProperty(result, 'name', { value: fn.name });
-      return result;
-    },
+    create,
     get,
+    and,
     query: queryServer,
   };
 };
