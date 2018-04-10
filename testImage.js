891f97643baa7ebbbbe6ed1194cb16c2d58fcec9
Domas <domas.bitvinskas@me.com>
2018-04-10 22:02:00 +0300

    Make it almost work

--- testImage.js
+++ testImage.js
@@ -1,8 +1,75 @@
 const gql = require('graphql-tag')
 const _ = require('lodash');
 const future = require('./index');
+const brain = require('brain.js');
 
 const run = async () => {
+  const images = [
+    { pixels: [{ r: 0.03, g: 0.7, b: 0.5 }] },
+    { pixels: [{ r: 0.16, g: 0.09, b: 0.2 }] },
+    { pixels: [{ r: 0.5, g: 0.5, b: 1.0 }] },
+  ];
+
+  const imageLabels = [
+    { black: 1 },
+    { white: 1 },
+    { white: 1 },
+  ];
+
+  const train = ({ images, imageLabels }) => {
+    console.log('training on images', { images, imageLabels });
+    const net = new brain.NeuralNetwork();
+
+    const data = _.map(images, (image, i) => (
+      {
+        input: image.pixels,
+        output: imageLabels[i],
+      }
+    ));
+
+    console.log('data', data);
+
+    net.train(data);
+
+    return {
+      weights: net.toJSON(),
+    };
+  };
+
+  const colorContrast = ({ image, train: { weights } }) => {
+    console.log('wei', weights);
+    const net = new brain.NeuralNetwork();
+    net.fromJSON(weights);
+
+    console.log(net.run(image).black);
+    return {
+      value: net.run(image).black,
+    };
+  };
+
+  const cc = future().create(colorContrast, {
+    name: 'colorContrast',
+    train: {
+      fn: train,
+      data: {
+        images,
+        imageLabels,
+      },
+      input: {
+        images: '[Image]',
+        imageLabels: '[ImageLabel]',
+      },
+    },
+    input: {
+      image: 'Image',
+    },
+    output: {
+      value: 'Float',
+    },
+  });
+
+  console.log(await cc({ image: images[0] }));
+
   const human = ({ image: { pixels } }) => {
     return {
       // pretend that some extraction of coords is happening from imageData
